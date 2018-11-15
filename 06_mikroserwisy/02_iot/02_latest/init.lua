2bf4be976f65299ee99c8792803d6322eb573527
Michal Sobczak <michalasobczak@gmail.com>
2018-11-15 11:59:32 +0100

    converter

--- 06_mikroserwisy/02_iot/02_latest/init.lua
+++ 06_mikroserwisy/02_iot/02_latest/init.lua
@@ -15,16 +15,16 @@ tmr_connection:register(1000, tmr.ALARM_AUTO, function (t)
     print(wifi.sta.getip())
     t:unregister() 
     tmr_task:register(1000, tmr.ALARM_AUTO, function (t) 
-      -- print(adc.read(0))
+      print(adc.read(0))
       local addr = 'http://192.168.0.37:3344/messages'
-      print(addr)
+      -- print(addr)
       http.post(addr, nil,
                '{"adc":"' .. adc.read(0) .. '"}',
                function(code, data)
                  if (code < 0) then
                    print("HTTP request failed")
                  else
-                   print(code, data)
+                   -- print(code, data)
                  end
                end)
     end)
