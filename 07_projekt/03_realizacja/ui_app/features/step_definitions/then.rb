920eca5ab497f5cbc51928d78f5e3fcfd550463c
Michal Sobczak <michalasobczak@gmail.com>
2018-11-21 19:09:14 +0100

    QA Rails

---
+++ 07_projekt/03_realizacja/ui_app/features/step_definitions/then.rb
@@ -0,0 +1,19 @@
+Then(/^page should have notice "(.*?)"$/) do |arg1|
+  #puts page.body.inspect
+  assert page.has_content?(arg1)
+end
+
+
+Then(/^page should not have notice "(.*?)"$/) do |arg1|
+  assert !page.has_content?(arg1)
+end
+
+
+Then /^page should have elements by xpath "(.*)"$/ do |xpath|
+  page.all(:xpath, xpath)  
+end
+
+
+
+
+
---
+++ 07_projekt/03_realizacja/ui_app/features/users.feature
@@ -0,0 +1,6 @@
+Feature: Users
+
+	Scenario: List users
+		Given I am logged in as a user
+		Given I am on "/users" page
+
